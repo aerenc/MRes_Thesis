a8f7b42039bcd043b2efaa6e1dada19bb545e24c
Ali Eren Çamur <aerenc@users.noreply.github.com>
2021-06-22 17:25:02 +0200

    Update BLP_THESIS_MAINFILE.m

--- BLP_THESIS_MAINFILE.m
+++ BLP_THESIS_MAINFILE.m
@@ -6,6 +6,8 @@
 clc;
 clear;
 
+global x_jmt beta  alpha xi_jmt w_jmt gamma omega_jmt  Total
+
 rand('seed',2344)    % reset uniform random number generator
 randn('seed',2344)   % reset normal random number generator
 
@@ -14,8 +16,8 @@ randn('seed',2344)   % reset normal random number generator
 % Generating the data for logit case first:
 
 alpha = -2.5;                 % true alpha
-beta  = [4,5,2,1.5]';       % true betas
-gamma = [3,1,1,0.5]';         % true gamma
+beta  = [3,2,5,1.5]';         % true betas
+gamma = [5,1,3,0.5]';         % true gamma
 theta = [alpha; beta; gamma]; % true theta
 
 I     = 200;                  % Total number of consumers in each market
@@ -26,7 +28,7 @@ T     = 10;                   % Total time period
 Total = J*M*T;                % Total goods present a priori
 
 x_jmt_1 = ones(Total,1);
-x_jmt_2 = unifrnd(4,6,Total,1);
+x_jmt_2 = unifrnd(3,7,Total,1);
 x_jmt_3 = unifrnd(-1,5,Total,1);
 x_jmt_4 = unifrnd(0.5,2.5,Total,1);
 x_jmt   = [x_jmt_1 x_jmt_2 x_jmt_3 x_jmt_4]; % Generated observed product characteristics
@@ -58,32 +60,13 @@ for j = 1:J
 end
 
 xi_jmt    = xi_jmt(:);                 % unobserved product characteristics vector
-omega_jmt = normrnd(0,0.5,Total,1);    % unobserved costs
+omega_jmt = normrnd(5,1,Total,1);      % unobserved costs
 
 mc_jmt    = w_jmt * gamma + omega_jmt; % getting marginal costs
 
-p_jmt = rand(Total,1);                % prices (will be calculated)
-% 
-% tic
-% p_jmt = SolveforPrice_logit(x_jmt,beta,alpha,xi_jmt,w_jmt,gamma,omega_jmt,Total);
-% toc
+p_jmt = rand(Total,1);                 % prices (will be calculated)
 
-delta_mt          = x_jmt * beta + alpha .* p_jmt + xi_jmt; 
-nom               = exp(delta_mt);
-cumsum_delta_jmt  = cumsum(nom);
-cumsum_1          = cumsum_delta_jmt(15,:);
-index             = [15:15:3000]';
-totalsum          = diff(cumsum_delta_jmt(index,:)); 
-totalsum          = [cumsum_1;totalsum];  % mt total share of each j \in J_{mt}
-Totalsum          = repelem(totalsum,15); % stretching it out to 3000*1 (since we have 15 products in each market a priori)
-denom             = ones(Total,1) + Totalsum;
-s_jmt             = nom./denom;
-
-
-solveforprices = @(p_jmt) (log(p_jmt - (ones(Total,1)./(alpha.*(ones(Total,1) - s_jmt)))) - w_jmt * gamma - omega_jmt);
-
-%solveforprices = @(p) (log(p - (1./(alpha.*(1 - s_mt./(1+ repelem([cumsum(exp(x_jmt * beta + p .* alpha + xi_jmt))(15,:); diff(cumsum(exp(x_jmt*beta+p.*alpha+xi_jmt))([15:15:3000]',:))],15))))))) - w_jmt * gamma - omega_jmt);
-
+solveforprices = @(p_jmt) (log(p_jmt - (1./(alpha.*(1 - s_jmt(p_jmt))))) - w_jmt * gamma - omega_jmt);
 
 
 %opts      = optimset('Display','off');
@@ -92,10 +75,6 @@ opts    = optimset('Display','iter','TolCon',1E-16,'TolFun',1E-16,'TolX',1E-16);
 tic
 prices  = fsolve(solveforprices,rand(Total,1),opts);
 toc
-
-
-
-
 
 
 
