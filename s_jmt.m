9378ea95516203893239d4a67f8cd13d2cad4099
Ali Eren Çamur <aerenc@users.noreply.github.com>
2021-06-25 02:44:24 +0200

    Update s_jmt.m

--- s_jmt.m
+++ s_jmt.m
@@ -1,41 +1,27 @@
 function [s___jmt] = s_jmt(p)
 
-global x_jmt beta alpha xi_jmt Total J M T index index2 index3 inits
-
-
-%delta_jmt         = zeros(Total,1);
-%epsilon_ijmt      = evrnd(0,1,Total,1) - evrnd(0,1,Total,1);
-%delta_jmt         = x_jmt * beta - alpha .* p + xi_jmt;
-
+global x_jmt beta alpha xi_jmt Total J M T index index2 index3
 
 delta_jmt          = zeros(Total,1);
 delta_jmt(index,:) = 0;
 
-
 for z = 1: M*T
-delta_jmt(index2(z,1):index2(z,2),:)  = x_jmt(index2(z,1):index2(z,2),:) * beta - alpha .* p(index3(z,1):index3(z,2),:) + xi_jmt(index2(z,1):index2(z,2),:);
+delta_jmt(index2(z,1):index2(z,2),:)  = [ones(J-1,1) x_jmt(index3(z,1):index3(z,2),:)] * beta - alpha .* p(index3(z,1):index3(z,2),:) + xi_jmt(index3(z,1):index3(z,2),:);
 end
 
-
-%{
-delta_jmt(index,:)= 0; 
-%}
 nom               = exp(delta_jmt);
-%nom(index,:)     = 1;
 cumsum_delta_jmt  = cumsum(nom);
 cumsum_1          = cumsum_delta_jmt(J,:);
-
 totalsum          = diff(cumsum_delta_jmt(index,:)); 
 totalsum          = [cumsum_1;totalsum]; % M*T total share of each j \in J_{mt}
 Totalsum          = repelem(totalsum,J); % stretching it out to (J*  M*T)*1 (since we have J products in each market a priori)
 denom             = 1 + Totalsum;
 s__jmt            = nom./denom;
-% s__jmt(index,:) = 1 ./ (1 + totalsum);
+
 
 s___jmt  = zeros(Total-M*T,1);
 
 for i = 1: M*T
-    
 s___jmt(index3(i,1):index3(i,2))  = s__jmt(index2(i,1):index2(i,2),1); % getting shares of inside goods
 end
 
