88e5830ca4c003b1318cc3716f2bf2d525487195
Ali Eren Çamur <aerenc@users.noreply.github.com>
2021-06-27 19:51:55 +0200

    Update BLP_THESIS_MAINFILE.m

--- BLP_THESIS_MAINFILE.m
+++ BLP_THESIS_MAINFILE.m
@@ -2,11 +2,15 @@
 % SEVERAL APPROACHES TO UNOBSERVED PRODUCT CHARACTERISTICS WITH SIMULATED DATA UNDER LOGIT & RC SPECIFICATION
 % Ali Eren Camur
 % Tilburg University - CentER
+
 clc;
 clear;
-global x_jmt beta alpha xi_jmt Total J M T index index2 index3 inits adj m_x_jmt m_xi_jmt m_index m_index2 m_index3 adj
+
+global x_jmt beta alpha xi_jmt Total J M T index index2 index3 inits adj m_x_jmt m_xi_jmt m_index m_index2 m_index3 m2_x_jmt m2_xi_jmt
+
 rng('default');
-%% SIMULATION SETTING - Generating Data
+
+%% SIMULATION SETTING - Generating Unmanipulated Data
 % Generating the data for logit case first:
 
 alpha   = 2;                    % true alpha
@@ -32,10 +36,10 @@ for i = 2 : M*T
 end
 index3             = zeros(M*T,2);  % indexing optimizer prices
 index3(1,1)        = 1   ;
-index3(1,2)        = 19;
+index3(1,2)        = J-1;
 for i = 2 : M*T
    index3(i,1)     = index3(i-1,2)+1;
-   index3(i,2)     = index3(i,1) + 18;
+   index3(i,2)     = index3(i,1) + J-2;
 end
 
 x_jmt_1 = unifrnd(0,1,Total-M*T,1);
@@ -59,35 +63,57 @@ t       = normrnd(0,0.4,T,1);
 xi_jmt  = zeros(J-1,M,T);
 
 for   j = 1:J-1
-    for market = 1:M
-       for time = 1:T
-            xi_jmt(j,market,time) = x(j,1) + m(market,1) + t(time,1);
-       end
+  for market = 1:M
+    for time = 1:T
+        xi_jmt(j,market,time) = x(j,1) + m(market,1) + t(time,1);
     end
+  end
 end
 
 xi_jmt             = xi_jmt(:);                 % unobserved product characteristics vector
-omega_jmt          = normrnd(0,1,Total-M*T,1);      % unobserved costs
+omega_jmt          = normrnd(0,1,Total-M*T,1);  % unobserved costs
 mc_jmt             = [ones(Total-M*T,1) w_jmt] * gamma + omega_jmt; % getting marginal costs
 inits              = ones(Total-M*T,1);
 solveforprices     = @(ppp) (ppp -(1./(alpha.*(1 - s_jmt(ppp)))) - mc_jmt);
-%opts        = optimset('Algorithm ','interior-point ','Display ','iter ');
-%opts        = optimset('Display','off');
-opts         = optimset('Display','iter','TolCon',1E-8,'TolFun',1E-8,'TolX',1E-10,'MaxFunEvals',1000000000); %
+%opts              = optimset('Algorithm ','interior-point ','Display ','iter ');
+%opts              = optimset('Display','off');
+opts               = optimset('Display','iter','TolCon',1E-8,'TolFun',1E-8,'TolX',1E-10,'MaxFunEvals',1000000000);
+
 tic
 p_jmt        = fsolve(solveforprices,inits,opts); % this gives us prices
 toc
 
-s_jmt                = s_jmt(p_jmt);          % gives us inner market shares
+s_jmt                = s_jmt(p_jmt);              % gives us inner market shares
 cumsum_s_jmt         = cumsum(s_jmt);
 cumsum_s_jmt_1       = cumsum_s_jmt(J-1,:);
 marketwisesum_s_jmt  = diff(cumsum_s_jmt(indexx,:)); 
 marketwisesum_s_jmt  = [cumsum_s_jmt_1;marketwisesum_s_jmt]; 
 s0_jmt               = 1 - marketwisesum_s_jmt;
 
-% Constructing the BLP instruments: Note that we will have two types of
-% instruments: own product characteristics and characteristics sum of rivals' characteristics 
-% across markets and time periods
+%% UNMANIPULATED DATA IN 3D-ARRAY FORM:
+
+array_s_jmt     = reshape(s_jmt,J-1,M,T);         % market share in array form
+array_p_jmt     = reshape(p_jmt,J-1,M,T);         % prices in array form
+array_xi_jmt    = reshape(xi_jmt,J-1,M,T);        % unobs. prod. chars. in array form
+array_omega_jmt = reshape(omega_jmt,J-1,M,T);
+array_mc_jmt    = reshape(mc_jmt,J-1,M,T);
+array_x_jmt     = zeros(J-1,M,T,size(x_jmt,2));
+x11_jmt = x_jmt(:,1);
+x12_jmt = x_jmt(:,2);
+x13_jmt = x_jmt(:,3);
+array_x_jmt(:,:,:,1) = reshape(x11_jmt,J-1,M,T);
+array_x_jmt(:,:,:,2) = reshape(x12_jmt,J-1,M,T);  
+array_x_jmt(:,:,:,3) = reshape(x13_jmt,J-1,M,T);  
+array_w_jmt = zeros(J-1,M,T,size(w_jmt,2));
+w11_jmt = w_jmt(:,1);
+w12_jmt = w_jmt(:,2);
+w13_jmt = w_jmt(:,3);
+array_w_jmt(:,:,:,1) = reshape(w11_jmt,J-1,M,T);  
+array_w_jmt(:,:,:,2) = reshape(w12_jmt,J-1,M,T);  
+array_w_jmt(:,:,:,3) = reshape(w13_jmt,J-1,M,T);  
+
+%% CONSTRUCTING THE BLP INSTRUMENTS FOR UNMANIPULATED DATA: 
+% Note that we will have two types of instruments: own product characteristics and characteristics sum of rivals' characteristics across markets and time periods
 
 cumsum_z1_jmt     = cumsum(x_jmt);
 cumsum_z1         = cumsum_z1_jmt(J-1,:);
@@ -100,11 +126,10 @@ end
 
 Z_blp = [ones(Total-M*T,1) x_jmt rival_jmt x_jmt.^2 rival_jmt.^2]; %BLP instruments -> contains own & rivals prod. characteristics and their squares.
 
-
+%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% OLS ESTIMATION OF HOMOGENEOUS LOGIT MODEL %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 
 % Now we need to construct log(S_{jm}/S_{0m}) dependent variable:
 
@@ -117,7 +142,6 @@ end
 X            = [ones(Total-M*T,1) x_jmt p_jmt];              % stacking regressors together
 OLSestimates = (X'*X)\X'*dependentlogratio; % classical OLS formula giving us OLS estimates on aggregate Logit model
 
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% OLS ESTIMATION OF FIRST STAGE IV %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -129,8 +153,6 @@ Z = [ones(Total-M*T,1) w_jmt x_jmt];
 price_hat = Z*inv(Z'*Z)*Z'*p_jmt;               % part of price that has no correlation with error term
 Twoslsestimatesfirststage = inv(Z'*Z)*Z'*p_jmt; % First stage estimates of coefficients
 
-
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% 2SLS ESTIMATION OF HOMOGENEOUS LOGIT MODEL %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -141,54 +163,42 @@ Twoslsestimatesfirststage = inv(Z'*Z)*Z'*p_jmt; % First stage estimates of coeff
 X_hat = [ones(Total-M*T,1) x_jmt price_hat];
 Twoslsestimatessecondstage = inv(X_hat' * X_hat)*X_hat'*dependentlogratio;
 
+%% DATA MANIPULATION & RE-ESTIMATION:
 
-%% DATA MANIPULATION
+adj       = 5;                                        % 5 goods will be removed from each market & time period randomly as below:
 
-% putting all required data in array form:
+m_index   = [J-adj:J-adj:Total-adj*M*T]';             % indexing each good in every market M*T combination for manipulated case
 
-array_s_jmt     = reshape(s_jmt,J-1,M,T);         % market share in array form
-array_p_jmt     = reshape(p_jmt,J-1,M,T);         % prices in array form
-array_xi_jmt    = reshape(xi_jmt,J-1,M,T);       % unobs. prod. chars. in array form
-array_omega_jmt = reshape(omega_jmt,J-1,M,T);
-array_mc_jmt    = reshape(mc_jmt,J-1,M,T);
-array_x_jmt     = zeros(J-1,M,T,size(x_jmt,2));
-x11_jmt = x_jmt(:,1);
-x12_jmt = x_jmt(:,2);
-x13_jmt = x_jmt(:,3);
-array_x_jmt(:,:,:,1) = reshape(x11_jmt,J-1,M,T); % unobs. prod. chars. in array form
-array_x_jmt(:,:,:,2) = reshape(x12_jmt,J-1,M,T); % unobs. prod. chars. in array form
-array_x_jmt(:,:,:,3) = reshape(x13_jmt,J-1,M,T); % unobs. prod. chars. in array form
-array_w_jmt = zeros(J-1,M,T,size(w_jmt,2));
-w11_jmt = w_jmt(:,1);
-w12_jmt = w_jmt(:,2);
-w13_jmt = w_jmt(:,3);
-array_w_jmt(:,:,:,1) = reshape(w11_jmt,J-1,M,T); % unobs. prod. chars. in array form
-array_w_jmt(:,:,:,2) = reshape(w12_jmt,J-1,M,T); % unobs. prod. chars. in array form
-array_w_jmt(:,:,:,3) = reshape(w13_jmt,J-1,M,T); % unobs. prod. chars. in array form
+m_indexx  = [J-1-adj:J-1-adj:Total-M*T-adj*M*T]';     % indexing that will be used in calculating outside good share for manipulated case
 
+m_index2             = zeros(M*T,2);                  % indexing the inside goods in each market in matrix form for manipulated case
+m_index2(1,1)        = 1;
+m_index2(1,2)        = J-1-adj;
+for i = 2 : M*T
+    m_index2(i,1)    = m_index(i-1,1)+1;       
+    m_index2(i,2)    = m_index(i,1)-1; 
+end
 
+m_index3             = zeros(M*T,2);                  % indexing optimizer prices for manipulated case
+m_index3(1,1)        = 1;
+m_index3(1,2)        = J-adj-1;
+for i = 2 : M*T
+   m_index3(i,1)     = m_index3(i-1,2)+1;
+   m_index3(i,2)     = m_index3(i,1) + J-adj-2;
+end
 
-indexxx = [1:1:J-1]';                        % indexing from 1 to 19 (i.e. max. number of brands for all M*T)
+m_drop_index = zeros(adj,M*T);                        % these elements will be dropped
 
-adj = 5;
-willbedroppedelementindex = zeros(adj,M*T);  % these elements will be dropped
+for i = 1: M*T
+m_drop_index(:,i) = randperm(J-1,adj)';
+end
 
-manipulatingindex1 = randi([1 4],1,M*T);
-manipulatingindex2 = randi([5 8],1,M*T);
-manipulatingindex3 = randi([9 12],1,M*T);
-manipulatingindex4 = randi([13 16],1,M*T);
-manipulatingindex5 = randi([17 19],1,M*T);
-
-willbedroppedelementindex(1,:) =  manipulatingindex1;
-willbedroppedelementindex(2,:) =  manipulatingindex2;
-willbedroppedelementindex(3,:) =  manipulatingindex3;
-willbedroppedelementindex(4,:) =  manipulatingindex4;
-willbedroppedelementindex(5,:) =  manipulatingindex5;  
+% Now, we reshape the data into (J-1) * (M*T) matrices for each element of
+% x and w
 
 reshaped_x1_jmt = reshape(x_jmt(:,1),J-1,M*T); 
 reshaped_x2_jmt = reshape(x_jmt(:,2),J-1,M*T); 
 reshaped_x3_jmt = reshape(x_jmt(:,3),J-1,M*T); 
-
 reshaped_w1_jmt = reshape(w_jmt(:,1),J-1,M*T); 
 reshaped_w2_jmt = reshape(w_jmt(:,2),J-1,M*T); 
 reshaped_w3_jmt = reshape(w_jmt(:,3),J-1,M*T); 
@@ -197,28 +207,28 @@ reshaped_xi_jmt    = reshape(xi_jmt,J-1,M*T);
 reshaped_omega_jmt = reshape(omega_jmt,J-1,M*T);
 
 for  i = 1 : M*T
-    reshaped_x1_jmt(willbedroppedelementindex(:,i),i) = 0;
+    reshaped_x1_jmt(m_drop_index(:,i),i) = 0;
 end
 for  i = 1 : M*T
-    reshaped_x2_jmt(willbedroppedelementindex(:,i),i) = 0;
+    reshaped_x2_jmt(m_drop_index(:,i),i) = 0;
 end
 for  i = 1 : M*T
-    reshaped_x3_jmt(willbedroppedelementindex(:,i),i) = 0;
+    reshaped_x3_jmt(m_drop_index(:,i),i) = 0;
 end
 for  i = 1 : M*T
-    reshaped_w1_jmt(willbedroppedelementindex(:,i),i) = 0;
+    reshaped_w1_jmt(m_drop_index(:,i),i) = 0;
 end
 for  i = 1 : M*T
-    reshaped_w2_jmt(willbedroppedelementindex(:,i),i) = 0;
+    reshaped_w2_jmt(m_drop_index(:,i),i) = 0;
 end
 for  i = 1 : M*T
-    reshaped_w3_jmt(willbedroppedelementindex(:,i),i) = 0;
+    reshaped_w3_jmt(m_drop_index(:,i),i) = 0;
 end
 for  i = 1 : M*T
-    reshaped_xi_jmt(willbedroppedelementindex(:,i),i) = 0;
+    reshaped_xi_jmt(m_drop_index(:,i),i) = 0;
 end
 for  i = 1 : M*T
-    reshaped_omega_jmt(willbedroppedelementindex(:,i),i) = 0;
+    reshaped_omega_jmt(m_drop_index(:,i),i) = 0;
 end
 
 %%
@@ -227,7 +237,7 @@ end
 m_x1_jmt = reshaped_x1_jmt(:);
 m_x1_jmt = m_x1_jmt(m_x1_jmt~=0);
 m_x2_jmt = reshaped_x2_jmt(:);
-m_x2_jmt = reshaped_x1_jmt(m_x2_jmt~=0);
+m_x2_jmt = m_x2_jmt(m_x2_jmt~=0);
 m_x3_jmt = reshaped_x3_jmt(:);
 m_x3_jmt = m_x3_jmt(m_x3_jmt~=0);
 
@@ -250,41 +260,145 @@ m_omega_jmt = m_omega_jmt(m_omega_jmt~=0);
 
 m_mc_jmt = [ones(Total-M*T-adj*M*T,1) m_w_jmt] * gamma + m_omega_jmt; % getting marginal costs for manipulated data
 
+m_inits                = ones(Total-M*T-adj*M*T,1);
+m_solveforprices       = @(pp) (pp -(1./(alpha.*(1 - m_s_jmt(pp)))) - m_mc_jmt);
+%opts                  = optimset('Algorithm ','interior-point ','Display ','iter ');
+%opts                  = optimset('Display','off');
+opts                   = optimset('Display','iter','TolCon',1E-8,'TolFun',1E-8,'TolX',1E-10,'MaxFunEvals',1000000000); %
 
-
-m_index   = [J-adj:J-adj:Total-adj*M*T]';             % indexing each good in every market M*T combination for manipulated case
-m_indexx  = [J-1-adj:J-1-adj:Total-M*T-adj*M*T]';     
-
-m_index2             = zeros(M*T,2);             % indexing the inside goods in each market in matrix form for manipulated case
-m_index2(1,1)        = 1;
-m_index2(1,2)        = J-1-adj;
-for i = 2 : M*T
-    m_index2(i,1)    = m_index(i-1,1)+1;       
-    m_index2(i,2)    = m_index(i,1)-1; 
-end
-m_index3             = zeros(M*T,2);  % indexing optimizer prices
-m_index3(1,1)        = 1;
-m_index3(1,2)        = 14;
-for i = 2 : M*T
-   m_index3(i,1)     = m_index3(i-1,2)+1;
-   m_index3(i,2)     = m_index3(i,1) + 13;
-end
-
-m_inits              = ones(Total-M*T-adj*M*T,1);
-m_solveforprices     = @(pp) (pp -(1./(alpha.*(1 - m_s_jmt(pp)))) - m_mc_jmt);
-%opts        = optimset('Algorithm ','interior-point ','Display ','iter ');
-%opts        = optimset('Display','off');
-opts                 = optimset('Display','iter','TolCon',1E-8,'TolFun',1E-8,'TolX',1E-10,'MaxFunEvals',1000000000); %
 tic
-m_p_jmt              = fsolve(m_solveforprices,m_inits,opts); % this gives us prices
+m_p_jmt                = fsolve(m_solveforprices,m_inits,opts); % this gives us prices
 toc
 
 m_s_jmt                = m_s_jmt(m_p_jmt);          % gives us inner market shares
 cumsum_m_s_jmt         = cumsum(m_s_jmt);
-cumsum_m_s_jmt_1       = cumsum_s_jmt(J-1,:);
+cumsum_m_s_jmt_1       = cumsum_m_s_jmt(J-adj-1,:);
 marketwisesum_m_s_jmt  = diff(cumsum_m_s_jmt(m_indexx,:)); 
 marketwisesum_m_s_jmt  = [cumsum_m_s_jmt_1;marketwisesum_m_s_jmt]; 
 m_s0_jmt               = 1 - marketwisesum_m_s_jmt;
+
+%%
+% Bir onceki manipulation'da her market ve time period'dan random bir
+% sekilde 5 tane eleman eledim. Ama given bir markette bu good t'de exist
+% edip t+1'de etmeyip t+2'de ederse (ki onceki manipulation bunu mumkun
+% kiliyor) bu intuitive olarak pek makul olmayabilir. Simdi her M icin tum
+% time periodlarda ayni elemanlari dusurerek datayi bastan manipule
+% edecegiz. Her M icin adj = 5 devam ediyor
+
+mm2_drop_index = zeros(adj,M);                        % these elements will be dropped, now the dropped brands will be same for all time periods for each market
+
+for i = 1 : M
+mm2_drop_index(:,i) = randperm(J-1,adj)';
+end
+mm2_drop_index = [16,18,6,13,4,14,15,16,11,12,5,13,12,10,9,9,7,2,12,15;12,16,12,16,13,8,1,11,10,3,18,5,2,11,2,7,9,5,17,7;2,6,13,3,14,9,10,10,1,5,1,16,15,19,3,5,18,1,16,6;6,17,18,1,16,6,14,8,18,15,8,15,7,8,15,14,8,16,11,13;19,5,10,17,12,17,2,1,8,14,13,12,19,1,11,6,5,3,18,11];
+mm2_drop_index = sort(mm2_drop_index);
+
+m2_drop_index = zeros(adj,M*T);
+
+t_index = zeros(M,2);
+t_index(1,1) = 1;
+t_index(1,2) = T;
+
+for  i = 2:M
+   t_index(i,1) = t_index(i-1,2) + 1;
+   t_index(i,2) = t_index(i,1) + T - 1;
+end
+
+for i = 1 : M
+    m2_drop_index(:,i:M:M*T-M+i)  =     repmat(mm2_drop_index(:,i),1,10);
+end
+
+% Now, we reshape the data into (J-1) * (M*T) matrices for each element of
+% x and w
+
+m2reshaped_x1_jmt = reshape(x_jmt(:,1),J-1,M*T); 
+m2reshaped_x2_jmt = reshape(x_jmt(:,2),J-1,M*T); 
+m2reshaped_x3_jmt = reshape(x_jmt(:,3),J-1,M*T); 
+m2reshaped_w1_jmt = reshape(w_jmt(:,1),J-1,M*T); 
+m2reshaped_w2_jmt = reshape(w_jmt(:,2),J-1,M*T); 
+m2reshaped_w3_jmt = reshape(w_jmt(:,3),J-1,M*T); 
+
+m2reshaped_xi_jmt    = reshape(xi_jmt,J-1,M*T); 
+m2reshaped_omega_jmt = reshape(omega_jmt,J-1,M*T);
+
+for  i = 1 : M*T
+    m2reshaped_x1_jmt(m2_drop_index(:,i),i) = 0;
+end
+for  i = 1 : M*T
+    m2reshaped_x2_jmt(m2_drop_index(:,i),i) = 0;
+end
+for  i = 1 : M*T
+    m2reshaped_x3_jmt(m2_drop_index(:,i),i) = 0;
+end
+for  i = 1 : M*T
+    m2reshaped_w1_jmt(m2_drop_index(:,i),i) = 0;
+end
+for  i = 1 : M*T
+    m2reshaped_w2_jmt(m2_drop_index(:,i),i) = 0;
+end
+for  i = 1 : M*T
+    m2reshaped_w3_jmt(m2_drop_index(:,i),i) = 0;
+end
+for  i = 1 : M*T
+    m2reshaped_xi_jmt(m2_drop_index(:,i),i) = 0;
+end
+for  i = 1 : M*T
+    m2reshaped_omega_jmt(m2_drop_index(:,i),i) = 0;
+end
+
+%%
+% m2_ stands for manipulated for our second case, i.e. when randomly chosen equal number (#: 5) of brands are exempted from all time periods of a given market:
+
+m2_x1_jmt = m2reshaped_x1_jmt(:);
+m2_x1_jmt = m2_x1_jmt(m2_x1_jmt~=0);
+m2_x2_jmt = m2reshaped_x2_jmt(:);
+m2_x2_jmt = m2_x2_jmt(m2_x2_jmt~=0);
+m2_x3_jmt = m2reshaped_x3_jmt(:);
+m2_x3_jmt = m2_x3_jmt(m2_x3_jmt~=0);
+
+m2_x_jmt = [m2_x1_jmt m2_x2_jmt m2_x3_jmt] ;
+
+m2_w1_jmt = m2reshaped_w1_jmt(:);
+m2_w1_jmt = m2_w1_jmt(m2_w1_jmt~=0);
+m2_w2_jmt = m2reshaped_w2_jmt(:);
+m2_w2_jmt = m2_w2_jmt(m2_w2_jmt~=0);
+m2_w3_jmt = m2reshaped_w3_jmt(:);
+m2_w3_jmt = m2_w3_jmt(m2_w3_jmt~=0);
+
+m2_w_jmt = [m2_w1_jmt m2_w2_jmt m2_w3_jmt];
+
+m2_xi_jmt = m2reshaped_xi_jmt(:);
+m2_xi_jmt = m2_xi_jmt(m2_xi_jmt~=0);
+
+m2_omega_jmt = m2reshaped_omega_jmt(:);
+m2_omega_jmt = m2_omega_jmt(m2_omega_jmt~=0);
+
+m2_mc_jmt = [ones(Total-M*T-adj*M*T,1) m2_w_jmt] * gamma + m2_omega_jmt; % getting marginal costs for manipulated data
+
+m2_inits               = ones(Total-M*T-adj*M*T,1);
+m2_solveforprices      = @(pp) (pp -(1./(alpha.*(1 - m2_s_jmt(pp)))) - m2_mc_jmt);
+%opts                  = optimset('Algorithm ','interior-point ','Display ','iter ');
+%opts                  = optimset('Display','off');
+opts                   = optimset('Display','iter','TolCon',1E-8,'TolFun',1E-8,'TolX',1E-10,'MaxFunEvals',1000000000); %
+
+tic
+m2_p_jmt               = fsolve(m2_solveforprices,m2_inits,opts); % this gives us prices
+toc
+
+m2_s_jmt               = m2_s_jmt(m2_p_jmt);          % gives us inner market shares
+cumsum_m2_s_jmt        = cumsum(m2_s_jmt);
+cumsum_m2_s_jmt_1      = cumsum_m2_s_jmt(J-adj-1,:);
+marketwisesum_m2_s_jmt = diff(cumsum_m2_s_jmt(m_indexx,:)); 
+marketwisesum_m2_s_jmt = [cumsum_m2_s_jmt_1;marketwisesum_m2_s_jmt]; 
+m2_s0_jmt              = 1 - marketwisesum_m2_s_jmt;
+
+
+
+
+
+
+
+
 
 
 
