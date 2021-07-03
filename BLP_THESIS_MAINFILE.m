13fe69f72d3aed7024b997c2294f5f0b615d29cb
Ali Eren Çamur <aerencamur@gmail.com>
2021-07-03 04:43:47 +0200

    Update BLP_THESIS_MAINFILE.m

--- BLP_THESIS_MAINFILE.m
+++ BLP_THESIS_MAINFILE.m
@@ -24,233 +24,46 @@ J       = 20;                   % Initial total good + outside share in each mar
 M       = 20;                   % Total markets
 T       = 10;                   % Total time period
 Total   = J*M*T;                % Total goods present a priori, INCLUDING outside good
+adj     = 5;                    % 5 goods will be removed from each market & time period randomly as in manipulated case
 
 %% INDEXES:
 
-index   = [J:J:Total]';             % indexing each good in every market M*T combination
-indexx  = [J-1:J-1:Total-M*T]';     % indexing that will be used in calculating outside good share in this setting
+[index ,indexx ,index2 ,index3 ,m_index ,m_indexx ,m_index2 ,m_index3, m_drop_index] = indexes(J,M,T,Total,adj);
 
-index2             = zeros(M*T,2);  % indexing the inside goods in each market in matrix form
-index2(1,1)        = 1;
-index2(1,2)        = J-1;
+%% SIMULATING THE DATA:
 
-for i = 2 : M*T
-    index2(i,1)    = index(i-1,1)+1;       
-    index2(i,2)    = index(i,1)-1; 
-end
+[x_jmt ,w_jmt ,xi_jmt ,omega_jmt ,mc_jmt] = simulateDataUnmanipulated(J,M,T,Total,gamma);
 
-index3             = zeros(M*T,2);  % indexing optimizer prices and shares etc.
-index3(1,1)        = 1;
-index3(1,2)        = J-1;
 
-for i = 2 : M*T
-   index3(i,1)     = index3(i-1,2)+1;
-   index3(i,2)     = index3(i,1)+J-2;
-end
+%% SOLVING FOR SHARES & PRICES:
 
-adj       = 5;                                        % 5 goods will be removed from each market & time period randomly as in manipulated case
-
-m_index   = [J-adj:J-adj:Total-adj*M*T]';             % indexing each good in every market M*T combination for manipulated case
-
-m_indexx  = [J-1-adj:J-1-adj:Total-M*T-adj*M*T]';     % indexing that will be used in calculating outside good share for manipulated case
-
-m_index2             = zeros(M*T,2);                  % indexing the inside goods in each market in matrix form for manipulated case
-m_index2(1,1)        = 1;
-m_index2(1,2)        = J-1-adj;
-
-for i = 2 : M*T
-    m_index2(i,1)    = m_index(i-1,1)+1;       
-    m_index2(i,2)    = m_index(i,1)-1; 
-end
-
-m_index3             = zeros(M*T,2);                  % indexing optimizer prices for manipulated case
-m_index3(1,1)        = 1;
-m_index3(1,2)        = J-adj-1;
-
-for i = 2 : M*T
-   m_index3(i,1)     = m_index3(i-1,2)+1;
-   m_index3(i,2)     = m_index3(i,1) + J-adj-2;
-end
-
-mm_drop_index = zeros(adj,M);     % these elements will be dropped in manipulation: the dropped brands will be same for all time periods for each market
-
-for i = 1 : M
-mm_drop_index(:,i) = randperm(J-1,adj)';
-end
-
-mm_drop_index = [16,18,6,13,4,14,15,16,11,12,5,13,12,10,9,9,7,2,12,15;12,16,12,16,13,8,1,11,10,3,18,5,2,11,2,7,9,5,17,7;2,6,13,3,14,9,10,10,1,5,1,16,15,19,3,5,18,1,16,6;6,17,18,1,16,6,14,8,18,15,8,15,7,8,15,14,8,16,11,13;19,5,10,17,12,17,2,1,8,14,13,12,19,1,11,6,5,3,18,11];
-mm_drop_index = sort(mm_drop_index);
-
-m_drop_index  = zeros(adj,M*T);
-
-t_index        = zeros(M,2);
-t_index(1,1)   = 1;
-t_index(1,2)   = T;
-
-for  i = 2:M
-   t_index(i,1) = t_index(i-1,2) + 1;
-   t_index(i,2) = t_index(i,1) + T - 1;
-end
-
-for i = 1 : M
-    m_drop_index(:,i:M:M*T-M+i) = repmat(mm_drop_index(:,i),1,T);
-end
-
-%% SIMULATING THE DATA
-
-% excluding the outside good <=> Total - M*T
-
-x_jmt_1 = unifrnd(0,1,Total-M*T,1);
-x_jmt_2 = unifrnd(0,1,Total-M*T,1);
-x_jmt_3 = unifrnd(0,1,Total-M*T,1);
-x_jmt   = [x_jmt_1 x_jmt_2 x_jmt_3];  % Generated observed product characteristics
-
-w_jmt_1 = unifrnd(0,1,Total-M*T,1);
-w_jmt_2 = unifrnd(0,1,Total-M*T,1);
-w_jmt_3 = unifrnd(0,1,Total-M*T,1);
-w_jmt   = [w_jmt_1 w_jmt_2 w_jmt_3];  % Generated cost shifters
-
-% Now, it's time to construct our unobserved product characteristics and
-% unobserved costs. We will introduce correlation across markets, across
-% time periods, and across the same good:
-
-x       = normrnd(0,1,J-1,1);
-m       = normrnd(0,0.2,M,1);
-t       = normrnd(0,0.4,T,1);
-
-xi_jmt  = zeros(J-1,M,T);
-
-for   j = 1:J-1
-  for market = 1:M
-    for time = 1:T
-        xi_jmt(j,market,time) = x(j,1) + m(market,1) + t(time,1);
-    end
-  end
-end
-
-xi_jmt               = xi_jmt(:);                                     % unobserved product characteristics vector
-omega_jmt            = normrnd(0,1,Total-M*T,1);                      % unobserved costs
-mc_jmt               = [ones(Total-M*T,1) w_jmt] * gamma + omega_jmt; % marginal cost specification
 inits                = ones(Total-M*T,1);
 solveforprices       = @(ppp) (ppp -(1./(alpha.*(1 - shares(ppp,x_jmt,xi_jmt,index,index2,index3,0)))) - mc_jmt);
-%opts                = optimset('Algorithm ','interior-point ','Display ','iter ');
-% opts               = optimset('Display','off');
 opts                 = optimset('Display','iter','TolCon',1E-8,'TolFun',1E-8,'TolX',1E-10,'MaxFunEvals',1000000000);
 
 tic
-p_jmt                = fsolve(solveforprices,inits,opts); % this gives us prices
+p_jmt                = fsolve(solveforprices,inits,opts);                    % this gives us prices
 toc
 
-s_jmt                = shares(p_jmt,x_jmt,xi_jmt,index,index2,index3,0);                      % this gives us inner market shares
+s_jmt                = shares(p_jmt,x_jmt,xi_jmt,index,index2,index3,0);     % this gives us inner market shares
 cumsum_s_jmt         = cumsum(s_jmt);
 cumsum_s_jmt_1       = cumsum_s_jmt(J-1,:);
 marketwisesum_s_jmt  = diff(cumsum_s_jmt(indexx,:)); 
 marketwisesum_s_jmt  = [cumsum_s_jmt_1;marketwisesum_s_jmt]; 
-s0_mt                = 1 - marketwisesum_s_jmt;                                               % getting outside shares for each M*T combo
+s0_mt                = 1 - marketwisesum_s_jmt;                              % getting outside shares for each M*T combo
 
-%% UNMANIPULATED DATA IN 3D-ARRAY ((J-1)*M*T) FORM:
+%% UNMANIPULATED DATA IN 3D-ARRAY ((J-1)*M*T) FORM AND MATRIX FORM (dimensions:(J-1)*(M*T)):
 
-array_s_jmt     = reshape(s_jmt,J-1,M,T);         % market shares
-array_p_jmt     = reshape(p_jmt,J-1,M,T);         % prices 
-array_xi_jmt    = reshape(xi_jmt,J-1,M,T);        % unobs. prod. characteristics
-array_omega_jmt = reshape(omega_jmt,J-1,M,T);     % unobs. costs
-array_mc_jmt    = reshape(mc_jmt,J-1,M,T);        % marginal costs
+[array_s_jmt,array_p_jmt,array_xi_jmt,array_omega_jmt,array_mc_jmt,array_x_jmt,array_w_jmt,reshaped_x1_jmt,reshaped_x2_jmt,reshaped_x3_jmt,...
+ reshaped_w1_jmt,reshaped_w2_jmt,reshaped_w3_jmt] = reshapeDataUnmanipulated(J,M,T,s_jmt,p_jmt,xi_jmt,omega_jmt,mc_jmt,x_jmt,w_jmt);
 
-array_x_jmt     = zeros(J-1,M,T,size(x_jmt,2));   % observed prod. characteristics
-x11_jmt = x_jmt(:,1);
-x12_jmt = x_jmt(:,2);
-x13_jmt = x_jmt(:,3);
-array_x_jmt(:,:,:,1) = reshape(x11_jmt,J-1,M,T);
-array_x_jmt(:,:,:,2) = reshape(x12_jmt,J-1,M,T);  
-array_x_jmt(:,:,:,3) = reshape(x13_jmt,J-1,M,T);  
-array_w_jmt = zeros(J-1,M,T,size(w_jmt,2));       % observed cost shifters
-w11_jmt = w_jmt(:,1);
-w12_jmt = w_jmt(:,2);
-w13_jmt = w_jmt(:,3);
-array_w_jmt(:,:,:,1) = reshape(w11_jmt,J-1,M,T);  
-array_w_jmt(:,:,:,2) = reshape(w12_jmt,J-1,M,T);  
-array_w_jmt(:,:,:,3) = reshape(w13_jmt,J-1,M,T);  
 
-%% UNMANIPULATED DATA IN MATRIX FORM (dimensions:(J-1)*(M*T))
+%% MANIPULATION BEGINS -> GETTING MANIPULATED SIMULATED DATA
 
-reshaped_x1_jmt    = reshape(x_jmt(:,1),J-1,M*T); 
-reshaped_x2_jmt    = reshape(x_jmt(:,2),J-1,M*T); 
-reshaped_x3_jmt    = reshape(x_jmt(:,3),J-1,M*T); 
-reshaped_w1_jmt    = reshape(w_jmt(:,1),J-1,M*T); 
-reshaped_w2_jmt    = reshape(w_jmt(:,2),J-1,M*T); 
-reshaped_w3_jmt    = reshape(w_jmt(:,3),J-1,M*T); 
-reshaped_xi_jmt    = reshape(xi_jmt,J-1,M*T); 
-reshaped_omega_jmt = reshape(omega_jmt,J-1,M*T);
+[m_x_jmt,m_w_jmt,m_xi_jmt,m_omega_jmt,m_mc_jmt] = simulateDataManipulated(Total,J,M,T,adj,x_jmt,w_jmt,xi_jmt,omega_jmt,m_drop_index,gamma);
 
-%% MANIPULATION BEGINS
 
-% Now, we drop 5 elements for each market for every time period randomly:
-
-% First, we reshape the data into (J-1) * (M*T) matrices for each element of
-% x and w and dropping the elements accordingly to m_drop_index that we
-% constructed in INDEXES subsection above:
-
-m_reshaped_x1_jmt    = reshape(x_jmt(:,1),J-1,M*T); 
-m_reshaped_x2_jmt    = reshape(x_jmt(:,2),J-1,M*T); 
-m_reshaped_x3_jmt    = reshape(x_jmt(:,3),J-1,M*T); 
-m_reshaped_w1_jmt    = reshape(w_jmt(:,1),J-1,M*T); 
-m_reshaped_w2_jmt    = reshape(w_jmt(:,2),J-1,M*T); 
-m_reshaped_w3_jmt    = reshape(w_jmt(:,3),J-1,M*T); 
-m_reshaped_xi_jmt    = reshape(xi_jmt,J-1,M*T); 
-m_reshaped_omega_jmt = reshape(omega_jmt,J-1,M*T);
-
-for  i = 1 : M*T
-    m_reshaped_x1_jmt(m_drop_index(:,i),i) = 0;
-end
-for  i = 1 : M*T
-    m_reshaped_x2_jmt(m_drop_index(:,i),i) = 0;
-end
-for  i = 1 : M*T
-    m_reshaped_x3_jmt(m_drop_index(:,i),i) = 0;
-end
-for  i = 1 : M*T
-    m_reshaped_w1_jmt(m_drop_index(:,i),i) = 0;
-end
-for  i = 1 : M*T
-    m_reshaped_w2_jmt(m_drop_index(:,i),i) = 0;
-end
-for  i = 1 : M*T
-    m_reshaped_w3_jmt(m_drop_index(:,i),i) = 0;
-end
-for  i = 1 : M*T
-    m_reshaped_xi_jmt(m_drop_index(:,i),i) = 0;
-end
-for  i = 1 : M*T
-    m_reshaped_omega_jmt(m_drop_index(:,i),i) = 0;
-end
-
-% m_ <=> manipulated:
-
-m_x1_jmt = m_reshaped_x1_jmt(:);
-m_x1_jmt = m_x1_jmt(m_x1_jmt~=0);
-m_x2_jmt = m_reshaped_x2_jmt(:);
-m_x2_jmt = m_x2_jmt(m_x2_jmt~=0);
-m_x3_jmt = m_reshaped_x3_jmt(:);
-m_x3_jmt = m_x3_jmt(m_x3_jmt~=0);
-
-m_x_jmt = [m_x1_jmt m_x2_jmt m_x3_jmt] ;
-
-m_w1_jmt = m_reshaped_w1_jmt(:);
-m_w1_jmt = m_w1_jmt(m_w1_jmt~=0);
-m_w2_jmt = m_reshaped_w2_jmt(:);
-m_w2_jmt = m_w2_jmt(m_w2_jmt~=0);
-m_w3_jmt = m_reshaped_w3_jmt(:);
-m_w3_jmt = m_w3_jmt(m_w3_jmt~=0);
-
-m_w_jmt = [m_w1_jmt m_w2_jmt m_w3_jmt];
-
-m_xi_jmt = m_reshaped_xi_jmt(:);
-m_xi_jmt = m_xi_jmt(m_xi_jmt~=0);
-
-m_omega_jmt = m_reshaped_omega_jmt(:);
-m_omega_jmt = m_omega_jmt(m_omega_jmt~=0);
-
-m_mc_jmt = [ones(Total-M*T-adj*M*T,1) m_w_jmt] * gamma + m_omega_jmt;                    % getting marginal costs for manipulated data
+%% SOLVING FOR SHARES & PRICES FOR MANIPULATED CASE:
 
 m_inits               = ones(Total-M*T-adj*M*T,1);
 m_solveforprices      = @(pp) (pp -(1./(alpha.*(1 - shares(pp,m_x_jmt,m_xi_jmt,m_index,m_index2,m_index3,adj)))) - m_mc_jmt);
@@ -266,44 +79,10 @@ marketwisesum_m_s_jmt = diff(cumsum_m_s_jmt(m_indexx,:));
 marketwisesum_m_s_jmt = [cumsum_m_s_jmt_1;marketwisesum_m_s_jmt]; 
 m_s0_mt               = 1 - marketwisesum_m_s_jmt;              % this gives us market shares of outside good in each market and time period
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% OLS ESTIMATION OF HOMOGENEOUS LOGIT MODEL FOR MANIPULATED DATA %%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% OLS ESTIMATION OF HOMOGENEOUS LOGIT MODEL FOR MANIPULATED DATA & OLS ESTIMATION OF FIRST STAGE IV FOR MANIPULATED DATA & 2SLS ESTIMATION OF HOMOGENEOUS LOGIT MODEL FOR MANIPULATED DATA
 
-% Now we need to construct log(S_{jm}/S_{0m}) dependent variable:
-
-m_dependentlogratio = zeros(Total-M*T-adj*M*T,1);
-
-for i = 1 : M*T
-    m_dependentlogratio(m_index3(i,1):m_index3(i,2),:) = log(m_s_jmt(m_index3(i,1):m_index3(i,2),:))-log(m_s0_mt(i,1));
-end
-
-m_X            = [ones(Total-M*T-adj*M*T,1) m_x_jmt m_p_jmt];   % stacking regressors together
-m_OLSestimates = (m_X'*m_X)\m_X'*m_dependentlogratio;           % classical OLS formula giving us OLS estimates on aggregate Logit model
-
-m_xi_jmt_error = m_dependentlogratio - m_X * m_OLSestimates ;   % This gives estimated unobserved product characteristics
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% OLS ESTIMATION OF FIRST STAGE IV FOR MANIPULATED DATA %%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% First, to be able to remove the endogeneity bias in our model, we regress
-% price additionally on cost shifters and collect the corresponding estimates for prices
-
-m_Z = [ones(Total-M*T-adj*M*T,1) m_x_jmt m_w_jmt];                                 
-m_price_hat = m_Z/(m_Z'*m_Z)*m_Z'*m_p_jmt;               % part of price that has no correlation with error term
-m_Twoslsestimatesfirststage = (m_Z'*m_Z)\m_Z'*m_p_jmt;   % first stage estimates of coefficients
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% 2SLS ESTIMATION OF HOMOGENEOUS LOGIT MODEL FOR MANIPULATED DATA %%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% Since we have the estimates for prices, we can now estimate our model by
-% also estimating the second stage of IV regression:
-
-m_X_hat = [ones(Total-M*T-adj*M*T,1) m_x_jmt m_price_hat];
-m_Twoslsestimatessecondstage = (m_X_hat' * m_X_hat)\m_X_hat'*m_dependentlogratio;
+[m_OLSestimates,m_xi_jmt_error,m_Twoslsestimatessecondstage] = OLS_IV_estimation_Manipulated(Total,M,T,adj,m_index3,m_s_jmt,m_s0_mt,m_x_jmt,m_p_jmt,...
+ m_w_jmt);
 
 %% INTRODUCTION OF EXISTING PRODUCT IN LOGIT CASE
 
