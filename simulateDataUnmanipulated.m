bb16afd1838b20672ed701872a6b976ac2807378
Ali Eren Çamur <aerencamur@gmail.com>
2021-07-10 18:20:59 +0300

    Update simulateDataUnmanipulated.m

--- simulateDataUnmanipulated.m
+++ simulateDataUnmanipulated.m
@@ -16,16 +16,20 @@ w_jmt   = [w_jmt_1 w_jmt_2 w_jmt_3];  % Generated cost shifters
 % unobserved costs. We will introduce correlation across markets, across
 % time periods, and across the same good:
 
-x       = normrnd(0,1,J-1,1);
-m       = normrnd(0,0.2,M,1);
-t       = normrnd(0,0.4,T,1);
+x       = normrnd(0,5,J-1,1);         % brand-specific components
+m       = normrnd(0,3.2,M,1);         % market-fixed effects
+t       = normrnd(0,1.6,T,1);         % year-fixed effects
 
 xi_jmt  = zeros(J-1,M,T);
+
+extra_noise = normrnd(0,1,J-1,M,T); % extra random shock
+
+% The above specification gives corr(p_jmt,xi_jmt) = 0.1312
 
 for   j = 1:J-1
   for market = 1:M
     for time = 1:T
-        xi_jmt(j,market,time) = x(j,1) + m(market,1) + t(time,1);
+        xi_jmt(j,market,time) = x(j,1) + m(market,1) + t(time,1) + extra_noise(j,market,time);
     end
   end
 end
@@ -33,5 +37,17 @@ end
 xi_jmt               = xi_jmt(:);                                     % unobserved product characteristics vector
 omega_jmt            = normrnd(0,1,Total-M*T,1);                      % unobserved costs
 mc_jmt               = [ones(Total-M*T,1) w_jmt] * gamma + omega_jmt; % marginal cost specification
+
+
+
+
+
+
+
+
+
+
+
+
 
 end
