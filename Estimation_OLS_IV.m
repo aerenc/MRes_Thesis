57baaf64e3f26c21f1b703a31a1f1a29b3a8f688
Ali Eren Çamur <aerencamur@gmail.com>
2021-07-20 21:01:03 +0300

    Update Estimation_OLS_IV.m

--- Estimation_OLS_IV.m
+++ Estimation_OLS_IV.m
@@ -12,10 +12,9 @@ for i = 1 : M*T
     y(index3(i,1):index3(i,2),:) = log(s_jmt(index3(i,1):index3(i,2),:))-log(s0_mt(i,1));
 end
 
-m_X          = [ones(Total-M*T-one,1) x_jmt p_jmt];                        % Stacking regressors together
-OLSestimates = (m_X'*m_X)\m_X'*y;                                          % Classical OLS formula giving us OLS estimates on aggregate Logit model
-
-xi_jmt_error = y - m_X * OLSestimates ;                                    % This gives estimated unobserved product characteristics
+X            = [ones(Total-M*T-one,1) x_jmt p_jmt];                        % Stacking regressors together
+OLSestimates = (X'*X)\X'*y;                                                % Classical OLS formula giving us OLS estimates on aggregate Logit model
+xi_jmt_error = y - X * OLSestimates ;                                      % This gives estimated unobserved product characteristics
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -25,9 +24,9 @@ xi_jmt_error = y - m_X * OLSestimates ;                                    % Thi
 % First, to be able to remove the endogeneity bias in our model, we regress
 % price additionally on cost shifters and collect the corresponding estimates for prices
 
-m_Z                         = [ones(Total-M*T-one,1) x_jmt w_jmt];                                 
-m_price_hat                 = m_Z/(m_Z'*m_Z)*m_Z'*p_jmt;                   % Part of price that has no correlation with error term
-m_Twoslsestimatesfirststage = (m_Z'*m_Z)\m_Z'*p_jmt;                       % First stage estimates of coefficients
+Z                          = [ones(Total-M*T-one,1) x_jmt w_jmt];                                 
+price_hat                  = Z/(Z'*Z)*Z'*p_jmt;                            % Part of price that has no correlation with error term
+Twoslsestimatesfirststage  = (Z'*Z)\Z'*p_jmt;                              % First stage estimates of coefficients
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% 2SLS ESTIMATION OF HOMOGENEOUS LOGIT MODEL FOR MANIPULATED DATA %%%
@@ -36,5 +35,5 @@ m_Twoslsestimatesfirststage = (m_Z'*m_Z)\m_Z'*p_jmt;                       % Fir
 % Since we have the estimates for prices, we can now estimate our model by
 % also estimating the second stage of IV regression:
 
-m_X_hat                    = [ones(Total-M*T-one,1) x_jmt m_price_hat];
-Twoslsestimatessecondstage = (m_X_hat' * m_X_hat)\m_X_hat'*y;
+X_hat                      = [ones(Total-M*T-one,1) x_jmt price_hat];
+Twoslsestimatessecondstage = (X_hat' * X_hat)\X_hat'*y;
