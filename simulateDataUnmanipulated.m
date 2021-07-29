23d7e5596aa3d5cf5ab2529ee8884ee9deccab4c
Ali Eren Çamur <aerencamur@gmail.com>
2021-07-29 03:34:11 +0300

    Update simulateDataUnmanipulated.m

--- simulateDataUnmanipulated.m
+++ simulateDataUnmanipulated.m
@@ -3,14 +3,24 @@ function [x_jmt, w_jmt, xi_jmt, omega_jmt, mc_jmt] = simulateDataUnmanipulated(J
 global x m t extra_noise
 % excluding the outside good <=> Total - M*T
 
-x_jmt_1 = unifrnd(0,1,Total-M*T,1);
-x_jmt_2 = unifrnd(0,1,Total-M*T,1);
-x_jmt_3 = unifrnd(0,1,Total-M*T,1);
+% x_jmt_1 = unifrnd(0,1,Total-M*T,1);
+% x_jmt_2 = unifrnd(0,1,Total-M*T,1);
+% x_jmt_3 = unifrnd(0,1,Total-M*T,1);
+% x_jmt   = [x_jmt_1 x_jmt_2 x_jmt_3];                                       % Generated observed product characteristics
+% 
+% w_jmt_1 = unifrnd(0,1,Total-M*T,1);
+% w_jmt_2 = unifrnd(0,1,Total-M*T,1);
+% w_jmt_3 = unifrnd(0,1,Total-M*T,1);
+% w_jmt   = [w_jmt_1 w_jmt_2 w_jmt_3];                                       % Generated cost shifters
+
+x_jmt_1 = normrnd(0,1,Total-M*T,1);
+x_jmt_2 = normrnd(0,1,Total-M*T,1);
+x_jmt_3 = normrnd(0,1,Total-M*T,1);
 x_jmt   = [x_jmt_1 x_jmt_2 x_jmt_3];                                       % Generated observed product characteristics
 
-w_jmt_1 = unifrnd(0,1,Total-M*T,1);
-w_jmt_2 = unifrnd(0,1,Total-M*T,1);
-w_jmt_3 = unifrnd(0,1,Total-M*T,1);
+w_jmt_1 = normrnd(0,1,Total-M*T,1);
+w_jmt_2 = normrnd(0,1,Total-M*T,1);
+w_jmt_3 = normrnd(0,1,Total-M*T,1);
 w_jmt   = [w_jmt_1 w_jmt_2 w_jmt_3];                                       % Generated cost shifters
 
 % x_jmt_1 = normrnd(0,1,Total-M*T,1);
@@ -44,9 +54,9 @@ w_jmt   = [w_jmt_1 w_jmt_2 w_jmt_3];                                       % Gen
 % unobserved costs. We will introduce correlation across markets, across
 % time periods, and across the same good:
 
-x       = normrnd(0,3,J-1,1);                                              % Brand-specific components
-m       = normrnd(0,2.4,M,1);                                            % Market-fixed effects
-t       = normrnd(0,1.8,T,1);                                            % Year-fixed effects
+x       = normrnd(0,1,J-1,1);                                              % Brand-specific components
+m       = normrnd(0,0.8,M,1);                                            % Market-fixed effects
+t       = normrnd(0,0.6,T,1);                                            % Year-fixed effects
 
 xi_jmt  = zeros(J-1,M,T);
 
