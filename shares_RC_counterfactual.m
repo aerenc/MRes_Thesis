1faa61c667aa001394d0a4748df90cde7096121c
Ali Eren Çamur <aerencamur@gmail.com>
2021-07-30 02:34:53 +0300

    Update shares_RC_counterfactual.m

--- shares_RC_counterfactual.m
+++ shares_RC_counterfactual.m
@@ -1,33 +1,35 @@
-function [inside_s_jmt] = shares_RC_counterfactual(p,base_x_jmt,xi_J,xi_vals)
+function [inside_s_jmt] = shares_RC_counterfactual(p,xi_vals)
 
-global beta_est alpha_est J I heterogeneity sigma_est  
+global beta_est alpha_est J I heterogeneity sigma_est base_x_jmt xi_J
 
-delta_jmt = zeros(J,I);
+delta_mu_jmt = zeros(J,I);
 
-delta_jmt(J,:) = 0;
+delta_mu_jmt(J,:) = 0;
 
 for i = 1:I
-delta_jmt(1,i) =  [1 base_x_jmt(1,:)] * beta_est + alpha_est .* p(1,:) + xi_vals - p(1,:)*sigma_est*heterogeneity(1,i);
+delta_mu_jmt(1,i) =  [1 base_x_jmt(1,:)] * beta_est - alpha_est .* p(1,:) + xi_vals - p(1,:)*sigma_est*heterogeneity(1,i);
 end
 
 for i = 1:I
-delta_jmt(2:J-1,i)  = [ones(J-2,1) base_x_jmt(2:J-1,:)] * beta_est ...
-    + alpha_est .* p(2:end,:) + xi_J(1:J-2,:)...
+delta_mu_jmt(2:J-1,i)  = [ones(J-2,1) base_x_jmt(2:J-1,:)] * beta_est ...
+    - alpha_est .* p(2:end,:) + xi_J(1:J-2,:)...
   - p(2:end,:)*sigma_est*heterogeneity(1,i); 
 end
 
 
-nom               = exp(delta_jmt);
+nom               = exp(delta_mu_jmt);
 cums = cumsum(nom,1);
 cums = cums(end,:);
 denom             = cums;
-s__jmt            = nom./denom;                                            
+s_ijmt            = nom./denom;                                            
 
-s___jmt           = mean(s__jmt,2);
+nom_general       = mean(s_ijmt,2);
+denom_general     = mean((alpha_est+sigma_est.*heterogeneity).*s_ijmt.*(1-s_ijmt),2); %BURADAKI ALPHA NEGATIF AMA POZITIF OLMASI GEREK -> DONE
+share_general     = nom_general./denom_general; 
 
-inside_s_jmt      = zeros(J-1,1);                  % Now, getting shares of inside goods
+inside_s_jmt      = zeros(J-1,1);                  
 
-inside_s_jmt(1:J-1,:)  = s___jmt(1:J-1,:);         % Getting shares of inside goods
+inside_s_jmt(1:J-1,:)  = share_general(1:J-1,:);         % Getting shares of inside goods
 
 
 
