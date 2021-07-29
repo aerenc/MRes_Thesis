50e980f016e1a405f2b69d42de1173eae0c5d617
Ali Eren Çamur <aerencamur@gmail.com>
2021-07-30 02:34:56 +0300

    Create sharescalculator_RC.m

---
+++ sharescalculator_RC.m
@@ -0,0 +1,48 @@
+function [inside_shares] = sharescalculator_RC(p,x_jmt,xi_jmt,sigmaa,index,index2,index3)
+
+global beta alpha Total J M T I heterogeneity
+% p   = normrnd(5,1,Total-M*T-1,1);
+
+delta_mu_jmt          = zeros(Total-1,I);                                  % This will be filled with mean market values
+delta_mu_jmt(index,:) = 0;
+
+for i = 1:I
+delta_mu_jmt(index2(1,1):index2(1,2),i)  = [ones(J-2,1) x_jmt(index3(1,1):index3(1,2),:)] * beta ...
+                                                     - alpha .* p(index3(1,1):index3(1,2),:) + xi_jmt(index3(1,1):index3(1,2),:)...
+  - p(index3(1,1):index3(1,2),:)*sigmaa(1)*heterogeneity(1,i); 
+end
+                                                                     % Since this specific M*T
+                                                                     % market lacks
+                                                                     % one good
+                                                                     % compared to
+                                                                     % the other markets
+                                                                      
+for k = 2:M*T
+    for i = 1:I
+                delta_mu_jmt(index2(k,1):index2(k,2),i)  = [ones(J-1,1) x_jmt(index3(k,1):index3(k,2),:)] * beta ...
+                                                     - alpha .* p(index3(k,1):index3(k,2),:) + xi_jmt(index3(k,1):index3(k,2),:)...
+  - p(index3(k,1):index3(k,2),:)*sigmaa(1)*heterogeneity(1,i);   
+    end
+end
+
+
+
+nom               = exp(delta_mu_jmt);
+cumsum_delta_jmt  = cumsum(nom,1);
+cumsum_1          = cumsum_delta_jmt(J-1,:);
+totalsum          = diff(cumsum_delta_jmt(index,:),1,1); 
+totalsum          = [cumsum_1;totalsum];                                   % M*T total share of each j \in J_{mt}
+Totalsum          = repelem(totalsum,J,1);                                 % Stretching it out to (J*M*T)*1 (since we have J-1 products in each market)
+Totalsum          = Totalsum(2:end,:);
+denom             = Totalsum;
+s__jmt            = nom./denom;                                            % Getting ALL shares, i.e. including s0_mt 
+
+s___jmt           = mean(s__jmt,2);
+
+inside_shares      = zeros(Total-M*T-1,1);                                  % Now, getting shares of inside goods
+
+for i = 1: M*T
+inside_shares(index3(i,1):index3(i,2))  = s___jmt(index2(i,1):index2(i,2),1);% Getting shares of inside goods
+end
+
+end
