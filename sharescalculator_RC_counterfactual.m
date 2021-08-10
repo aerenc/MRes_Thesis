f41435e7bf47a1514935982957d386a65fe7ca53
Ali Eren Çamur <aerencamur@gmail.com>
2021-08-10 03:17:02 +0300

    Update sharescalculator_RC_counterfactual.m

--- sharescalculator_RC_counterfactual.m
+++ sharescalculator_RC_counterfactual.m
@@ -1,19 +1,19 @@
 function [inside_shares] = sharescalculator_RC_counterfactual(p,xi_vals)
 
-global beta_est alpha_est J I heterogeneity sigma_est base_x_jmt xi_J
+global beta_est alpha_est J sigma_est base_x_jmt xi_J v ns
 
-delta_mu_jmt = zeros(J,I);
+delta_mu_jmt = zeros(J,ns);
 
 delta_mu_jmt(J,:) = 0;
 
-for i = 1:I
-delta_mu_jmt(1,i) =  [1 base_x_jmt(1,:)] * beta_est - alpha_est .* p(1,:) + xi_vals - p(1,:)*sigma_est*heterogeneity(1,i);
+for i = 1:ns
+delta_mu_jmt(1,i) =  [1 base_x_jmt(1,:)] * beta_est - alpha_est .* p(1,:) + xi_vals - p(1,:)*sigma_est*v(1,i);
 end
 
-for i = 1:I
+for i = 1:ns
 delta_mu_jmt(2:J-1,i)  = [ones(J-2,1) base_x_jmt(2:J-1,:)] * beta_est ...
     - alpha_est .* p(2:end,:) + xi_J(1:J-2,:)...
-  - p(2:end,:)*sigma_est*heterogeneity(1,i); 
+  - p(2:end,:)*sigma_est*v(1,i); 
 end
 
 
