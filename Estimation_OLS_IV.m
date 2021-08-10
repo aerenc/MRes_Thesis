9652a6d1140df5d8da7cb5cbf601ed1692a3787f
Ali Eren Çamur <aerencamur@gmail.com>
2021-08-10 03:16:08 +0300

    Update Estimation_OLS_IV.m

--- Estimation_OLS_IV.m
+++ Estimation_OLS_IV.m
@@ -1,4 +1,4 @@
-function  [OLSestimates,xi_jmt_error_logit,Twoslsestimatessecondstage,xi_jmt_error_twosls,se_ols,se_twosls] = Estimation_OLS_IV(Total,M,T,one,index3,s_jmt,s0_mt,x_jmt,p_jmt,w_jmt)
+function  [OLSestimates,xi_jmt_error_logit,Twoslsestimatessecondstage,xi_jmt_error_twosls,se_ols,se_twosls,pvalue_ols,pvalue_twosls] = Estimation_OLS_IV(Total,M,T,one,index3,s_jmt,s0_mt,x_jmt,p_jmt,w_jmt)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% OLS ESTIMATION OF HOMOGENEOUS LOGIT MODEL FOR "REAL" DATA %%%
@@ -14,13 +14,16 @@ end
 
 X            = [ones(Total-M*T-one,1) x_jmt p_jmt];                        % Stacking regressors together
 OLSestimates = (X'*X)\X'*y;                                                % Classical OLS formula giving us OLS estimates on aggregate Logit model
-xi_jmt_error_logit = y - X * OLSestimates ;                                      % This gives estimated unobserved product characteristics
+xi_jmt_error_logit = y - X * OLSestimates ;                                % This gives estimated unobserved product characteristics
 
 n_logit=size(X,1);
 k_logit=size(X,2);
 
 covbeta_ols=(xi_jmt_error_logit'*xi_jmt_error_logit/(n_logit-k_logit))*inv(X'*X);
 se_ols=sqrt(diag(covbeta_ols));
+
+pvalue_ols=2*(1-tcdf(abs(OLSestimates./se_ols),n_logit-k_logit));
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% OLS ESTIMATION OF FIRST STAGE IV FOR "REAL" DATA %%%
@@ -42,10 +45,12 @@ Twoslsestimatesfirststage  = (Z'*Z)\Z'*p_jmt;                              % Fir
 
 X_hat                      = [ones(Total-M*T-one,1) x_jmt price_hat];
 Twoslsestimatessecondstage = (X_hat' * X_hat)\X_hat'*y;
-xi_jmt_error_twosls = y - X_hat * Twoslsestimatessecondstage ;                                      % This gives estimated unobserved product characteristics
+xi_jmt_error_twosls = y - X_hat * Twoslsestimatessecondstage ;             % This gives estimated unobserved product characteristics
 
 n_twosls=size(X,1);
 k_twosls=size(X,2);
 
 covbeta_twosls=(xi_jmt_error_twosls'*xi_jmt_error_twosls/(n_twosls-k_twosls))*inv(X_hat'*X_hat);
 se_twosls=sqrt(diag(covbeta_twosls));
+
+pvalue_twosls=2*(1-tcdf(abs(Twoslsestimatessecondstage./se_twosls),n_twosls-k_twosls));
